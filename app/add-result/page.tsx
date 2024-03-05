fea88b4295e5b856bca344d7fc0bae6c47d1c442
falgun <falgundudhat25@gmail.com>
2024-03-05 14:25:00 -0500

    fix selection issue

--- app/add-result/page.tsx
+++ app/add-result/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 import {useDispatch} from "react-redux";
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import {addResult} from "@/lib/state/tunks"
 import {selectors as studentSelector} from "@/lib/state/slices/student";
 import {useAppSelector} from "@/lib/hooks";
 import {getCourses, getStudents} from "@/lib/state/tunks";
+import AppAlertContext from "@/app/AppAlertContext";
 
 export default function AddResult() {
     const dispatch = useDispatch();
+    const alert = useContext(AppAlertContext);
     const courses =  useAppSelector(studentSelector.getCoursesFromState);
     const students = useAppSelector(studentSelector.getStudentsFromState);
     const [student, setStudent] = useState('');
@@ -21,15 +23,21 @@ export default function AddResult() {
         fetchData();
     }, [dispatch]);
 
-    console.log(course)
-    console.log(student)
     return (
         <div
             className="flex flex-col flex-1 items-center">
             <h1 className="font-bold text-xl">Add Result</h1>
             <form
                 className="flex flex-col space-y-4 mx-auto mt-4 p-4 border border-gray-200 w-1/4"
-                onSubmit={() => dispatch<any>(addResult({student, course, score}))}>
+                onSubmit={async (e) => {
+                    e.preventDefault();
+                    const {error} = await dispatch<any>(addResult({student, course, score}))
+                    if (error) {
+                        alert.error(error.message);
+                        return;
+                    }
+                    alert.success('Result added successfully');
+                }}>
                 <div className="flex justify-around">
                 <label>
                     Student:
@@ -39,6 +47,11 @@ export default function AddResult() {
                         setStudent(e.target.value)
                     }}
                     >
+                        <option
+                            value=""
+                            disabled
+                            hidden
+                        > -- </option>
                         {students.map((student) => (
                             <option className="p-2" key={student._id} value={student._id}>
                                 {student.firstName}
@@ -55,6 +68,11 @@ export default function AddResult() {
                         e.preventDefault()
                         setCourse(e.target.value)
                     }}>
+                        <option
+                            value=""
+                            disabled
+                            hidden
+                        > -- </option>
                         {courses.map((course) => <option className="p-2" key={course._id} value={course._id}>{course.courseName}</option>)}
                     </select>
                 </div>
@@ -66,6 +84,11 @@ export default function AddResult() {
                         e.preventDefault()
                         setScore(e.target.value)
                     }}>
+                        <option
+                            value=""
+                            disabled
+                            hidden
+                        > -- </option>
                         <option value="A">A</option>
                         <option value="B">B</option>
                         <option value="C">C</option>
